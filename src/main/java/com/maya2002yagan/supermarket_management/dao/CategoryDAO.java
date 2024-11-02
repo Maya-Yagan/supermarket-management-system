03086c73d114a3101cf8ffcbc9fa690f3f8f5b5e
Maya-Yagan <maya2002yagan@gmail.com>
2024-11-03 01:12:55 +0300

    added dao classes for product and category

---
+++ src/main/java/com/maya2002yagan/supermarket_management/dao/CategoryDAO.java
@@ -0,0 +1,75 @@
+package com.maya2002yagan.supermarket_management.dao;
+
+import com.maya2002yagan.supermarket_management.model.Category;
+import com.maya2002yagan.supermarket_management.util.HibernateUtil;
+import java.util.HashSet;
+import java.util.Set;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+/**
+ * Data Access Object (DAO) for the Category entity.
+ * 
+ * @author Maya Yagan
+ */
+public class CategoryDAO {
+    public void insertCategory(Category category){
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            session.save(category);
+            transaction.commit();
+        } catch (Exception e) {
+            if(transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+    
+    public Category getCategoryById(int id){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Category> query = session.createQuery("FROM Category c WHERE c.id = :id", Category.class);
+            query.setParameter("id", id);
+            return query.uniqueResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public Set<Category> getCategories(){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            Query<Category> query = session.createQuery("SELECT DISTINCT c FROM Category c", Category.class);
+            return new HashSet<>(query.getResultList());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public void updateCategory(Category category){
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            Category c = session.get(Category.class, category.getId());
+            c.setName(category.getName());
+            session.update(c);
+            transaction.commit();
+        } catch (Exception e){
+            if(transaction != null) transaction.rollback();
+          e.printStackTrace();
+        }
+    }
+    
+    public void deleteCategory(int id){
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            Category category = session.get(Category.class, id);
+            if(category != null) session.delete(category);
+            transaction.commit();
+        } catch (Exception e){
+            if(transaction != null) transaction.rollback();
+          e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/maya2002yagan/supermarket_management/dao/ProductDAO.java
@@ -0,0 +1,80 @@
+package com.maya2002yagan.supermarket_management.dao;
+
+import com.maya2002yagan.supermarket_management.model.Product;
+import com.maya2002yagan.supermarket_management.util.HibernateUtil;
+import java.util.HashSet;
+import java.util.Set;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+/**
+ * Data Access Object (DAO) for the Product entity.
+ * 
+ * @author Maya Yagan
+ */
+public class ProductDAO {
+    public void insertProduct(Product product){
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            session.save(product);
+            transaction.commit();
+        } catch (Exception e) {
+            if(transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+    
+    public Product getProductById(int id){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Product> query = session.createQuery("FROM Product p WHERE p.id = :id", Product.class);
+            query.setParameter("id", id);
+            return query.uniqueResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public Set<Product> getProducts(){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            Query<Product> query = session.createQuery("SELECT DISTINCT p FROM Product p", Product.class);
+            return new HashSet<>(query.getResultList());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public void updateProduct(Product product){
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            Product p = session.get(Product.class, product.getId());
+            p.setName(product.getName());
+            p.setPrice(product.getPrice());
+            p.setProductionDate(product.getProductionDate());
+            p.setExpirationDate(product.getExpirationDate());
+            p.setCategory(product.getCategory());
+            session.update(p);
+            transaction.commit();
+        } catch (Exception e){
+            if(transaction != null) transaction.rollback();
+          e.printStackTrace();
+        }
+    }
+    
+    public void deleteProduct(int id){
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            Product product = session.get(Product.class, id);
+            if(product != null) session.delete(product);
+            transaction.commit();
+        } catch (Exception e){
+            if(transaction != null) transaction.rollback();
+          e.printStackTrace();
+        }
+    }
+}
