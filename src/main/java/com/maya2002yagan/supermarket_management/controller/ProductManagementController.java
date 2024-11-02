4fe5602062cf8fb6c764c0780b102ac38576ffeb
Maya-Yagan <maya2002yagan@gmail.com>
2024-11-03 01:14:13 +0300

    added basic fxml file and controller class for product management page

---
+++ src/main/java/com/maya2002yagan/supermarket_management/controller/ProductManagementController.java
@@ -0,0 +1,22 @@
+package com.maya2002yagan.supermarket_management.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author maya2
+ */
+public class ProductManagementController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
